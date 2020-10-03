7cd9135bd07e3bd0e64c9b90556a3c81e163518d
Charith99IT <charithmadusankha99@gmail.com>
2020-10-04 01:49:43 +0530

    size perice change and qty increase added

--- app/src/main/java/com/example/pizzaexpress/Sides1_chickenWingsBbq.java
+++ app/src/main/java/com/example/pizzaexpress/Sides1_chickenWingsBbq.java
@@ -5,14 +5,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Sides1_chickenWingsBbq extends AppCompatActivity {
+
+    TextView qty;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sides1_chicken_wings_bbq);
+
+        qty = findViewById(R.id.itemQty);
 
         //back button redirect to the home page
         final ImageView backImgBtn = findViewById(R.id.backBtn_cat);
@@ -26,7 +34,49 @@ public class Sides1_chickenWingsBbq extends AppCompatActivity {
             }
         });
 
+    }
 
+    //Size selection method
+    public void onAlignmentSelected(View view) {
+        TextView price = findViewById(R.id.price);
+        RadioGroup radioGroup = findViewById(R.id.radioGroup);
+        switch (radioGroup.getCheckedRadioButtonId()) {
+            case R.id.size_small:
+                price.setText("Rs. 450");
+                break;
+            case R.id.size_medium:
+                price.setText("Rs. 800");
+                break;
+            case R.id.size_large:
+                price.setText("Rs. 1500");
+                break;
 
+        }
+    }
+
+    //increase quantity
+    public void increaseItemQty1(View view){
+        int temp;
+        String text;
+        temp = Integer.parseInt(qty.getText().toString());
+        temp ++;
+        text = String.valueOf(temp);
+        qty.setText(text);
+    }
+
+    //decrease quantity
+    public void decreaseItemQty(View view){
+
+        int temp;String text;
+
+        temp = Integer.parseInt(qty.getText().toString());
+        if (temp == 1){
+            Toast.makeText(this, "Qty should be at least 01", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            temp--;
+            text = String.valueOf(temp);
+            qty.setText(text);
+        }
     }
 }
